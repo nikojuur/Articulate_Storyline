3f46960ffe514548e13d82620ae25ed7d50ea753
nikojuur <njuwur@gmail.com>
2025-06-10 01:03:24 +0800

    first commit

---
+++ story_content/user.js
@@ -0,0 +1,19 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5oQUDnSY4oE":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = dokument.getElementByld('bgongku');
+audio.scr="musikku.mp3";
+audio.lord();
+audio.play();
+audio.volume=0,6;
+}
+
